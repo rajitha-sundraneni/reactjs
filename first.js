2349253ca0cb07605a20300ca066fd60b36cdcc5
unknown <sundranenirajitha@gmail.com>
2024-11-02 10:32:23 +0530

    this is first commit

---
+++ first.js
@@ -0,0 +1,4 @@
+import add, { div, mul} from "./mathope.js";
+console.log(add(10,20))
+console.log(mul(10,20))
+console.log(div(3,5))
---
+++ mathope.js
@@ -0,0 +1,17 @@
+function add(n1,n2)
+{
+    return n1+n1
+}
+export default add;
+
+export function mul(n1,n2)
+{
+    return n1*n2
+}
+
+export function div(n1,n2)
+{
+    return n1/n2
+}
+
+
